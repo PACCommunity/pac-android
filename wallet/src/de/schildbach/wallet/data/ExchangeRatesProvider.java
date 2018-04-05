1762a01037d54dc51c95aa6f89bdf55c6c04c271
Samuel Barbosa <sambarbosaa@gmail.com>
2018-04-05 20:17:56 -0300

    bug fixes

--- wallet/src/de/schildbach/wallet/data/ExchangeRatesProvider.java
+++ wallet/src/de/schildbach/wallet/data/ExchangeRatesProvider.java
@@ -265,9 +265,9 @@ public class ExchangeRatesProvider extends ContentProvider {
                                 && !fiatCurrencyCode.equals(MonetaryFormat.CODE_MBTC)
                                 && !fiatCurrencyCode.equals(MonetaryFormat.CODE_UBTC)) {
                             final JSONObject exchangeRate = head.getJSONObject(currencyCode);
-                            final JSONObject averages = exchangeRate.getJSONObject("averages");
+                            final String average = exchangeRate.getString("last");
                             try {
-                                Double _rate = pacPerBTC * averages.getDouble("day");
+                                Double _rate = pacPerBTC * Double.parseDouble(average);
                                 final Fiat rate = parseFiatInexact(fiatCurrencyCode, _rate.toString());
                                 if (rate.signum() > 0)
                                     rates.put(fiatCurrencyCode, new ExchangeRate(
